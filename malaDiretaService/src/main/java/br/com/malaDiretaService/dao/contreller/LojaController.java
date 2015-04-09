4b7e1cdffbcbda31f0c99752893f5ff4723a84f3
Cristian <Cristian@Cristian-PC>
2015-04-08 21:59:54 -0300

    Comitando template

--- malaDiretaService/src/main/java/br/com/malaDiretaService/dao/contreller/LojaController.java
+++ malaDiretaService/src/main/java/br/com/malaDiretaService/dao/contreller/LojaController.java
@@ -5,6 +5,7 @@ import java.io.Serializable;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 
+import br.com.malaDiretaService.model.Loja;
 import br.com.malaDiretaService.service.LojaService;
 
 
@@ -16,8 +17,22 @@ public class LojaController implements Serializable {
     
     private LojaService lojaService = new LojaService();
     
+    private Loja loja = new Loja();
+    
     public void teste() {
+    	loja.setCnpj("58.069.360/0001-20");
+    	loja.setNomeLoja("Loja V8");
+    	loja.setEmail("v8@lojas.com.br");
+    	loja.setTelefone("(32) 3041-0067");
     	lojaService.listarTodos();
+	}
+
+	public Loja getLoja() {
+		return loja;
+	}
+
+	public void setLoja(Loja loja) {
+		this.loja = loja;
 	}
     
 
